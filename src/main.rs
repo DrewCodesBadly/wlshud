5da4d055a52a32ccb7e104016b270f4506204ebb
DrewCodesBadly <dhruvak@outlook.com>
2026-03-11 21:23:38 -0400

    gtk4 layer shell setup

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,27 @@
 use std::{
     sync::mpsc,
     thread::{self},
-    time::{Duration, Instant},
 };
 
-use smithay_client_toolkit::{
-    compositor::CompositorState,
-    output::OutputState,
-    reexports::{
-        calloop::EventLoop,
-        calloop_wayland_source::WaylandSource,
-        client::{Connection, globals::registry_queue_init},
+use gtk4::{
+    Application, ApplicationWindow, Box, CenterBox, Frame, Label, MenuButton, SearchBar,
+    SearchEntry, Window,
+    builders::SearchBarBuilder,
+    gio::{
+        ActionEntry, SimpleActionGroup,
+        prelude::{ActionGroupExt, ActionMapExtManual, ApplicationExt, ApplicationExtManual},
     },
-    registry::RegistryState,
-    seat::SeatState,
-    shell::{
-        WaylandSurface,
-        wlr_layer::{Anchor, LayerShell},
-    },
-    shm::{Shm, slot::SlotPool},
+    glib::{self, clone},
+    prelude::{BoxExt, GtkApplicationExt, GtkWindowExt, WidgetExt},
 };
+use gtk4_layer_shell::LayerShell;
 
-use crate::{
-    rendering::{AppContext, create_app_fade_tween, layout::AppLayout},
-    searching::{SearchResults, searching_thread},
-    window::HUDWindow,
-};
+use crate::searching::searching_thread;
 
 mod config;
-mod rendering;
 mod searching;
-mod window;
+
+const APP_MARGIN: i32 = 32;
 
 fn main() {
     // Spawn searcher thread
@@ -38,79 +29,99 @@ fn main() {
     let (results_send, results_recv) = mpsc::channel();
     let (query_send, query_recv) = mpsc::channel();
 
-    let conn = Connection::connect_to_env().unwrap();
-    let (globals, event_queue) = registry_queue_init(&conn).unwrap();
-    let qh = event_queue.handle();
-    let mut event_loop: EventLoop<HUDWindow> = EventLoop::try_new().unwrap();
-    let loop_handle = event_loop.handle();
-    WaylandSource::new(conn.clone(), event_queue)
-        .insert(loop_handle.clone())
-        .unwrap();
-
-    let compositor = CompositorState::bind(&globals, &qh).unwrap();
-    let layer_shell =
-        LayerShell::bind(&globals, &qh).expect("Compositor does not support layer shells");
-    let surface = compositor.create_surface(&qh);
-    let layer_surface = layer_shell.create_layer_surface(
-        &qh,
-        surface,
-        smithay_client_toolkit::shell::wlr_layer::Layer::Top,
-        Some("WLSHUD"),
-        None,
-    );
-    layer_surface.set_anchor(Anchor::all());
-    layer_surface.set_keyboard_interactivity(
-        smithay_client_toolkit::shell::wlr_layer::KeyboardInteractivity::Exclusive,
-    );
-
-    layer_surface.commit();
-
-    let shm = Shm::bind(&globals, &qh).unwrap();
-    let pool = SlotPool::new(256 * 256 * 4, &shm).unwrap();
-
-    let mut window = HUDWindow {
-        shm,
-        pool,
-        layer_surface,
-        loop_handle,
-
-        registry_state: RegistryState::new(&globals),
-        seat_state: SeatState::new(&globals, &qh),
-        output_state: OutputState::new(&globals, &qh),
-        keyboard: None,
-        pointer: None,
-
-        width: 256,
-        height: 256,
-        buffer: None,
-
-        should_close: false,
-        in_closing_animation: false,
-
-        last_frame_time: Instant::now(),
-
-        app_fade_tweener: create_app_fade_tween(0., 1.),
-        app_fade_pos: 0.,
-
-        app_layout: AppLayout::default(),
-        app_context: AppContext::new(query_send),
-        search_results_receiver: results_recv,
-    };
-
     // Start searcher thread
     let _searching_thread = thread::spawn(move || searching_thread(results_send, query_recv));
 
-    // Run startup callback
-    window.app_layout.on_startup(&mut window.app_context);
+    let app = gtk4::Application::builder()
+        .application_id("com.github.DrewCodesBadly.wlshud")
+        .build();
+    app.connect_activate(activate);
 
-    // Run main event loop
-    loop {
-        event_loop
-            .dispatch(Duration::from_millis(10), &mut window)
-            .unwrap();
+    // Set binds
+    app.set_accels_for_action("wlshud.close", &["Escape"]);
 
-        if window.should_close {
-            break;
+    app.run();
+}
+
+fn activate(app: &Application) {
+    let window = gtk4::ApplicationWindow::new(app);
+
+    window.init_layer_shell();
+    window.set_layer(gtk4_layer_shell::Layer::Overlay);
+    window.set_keyboard_mode(gtk4_layer_shell::KeyboardMode::Exclusive);
+    // anchors to all 4 sides (take up whole screen)
+    window.set_anchor(gtk4_layer_shell::Edge::Bottom, true);
+    window.set_anchor(gtk4_layer_shell::Edge::Top, true);
+    window.set_anchor(gtk4_layer_shell::Edge::Left, true);
+    window.set_anchor(gtk4_layer_shell::Edge::Right, true);
+
+    // Actions
+    let close_action = ActionEntry::builder("close")
+        .activate(clone!(
+            #[weak]
+            window,
+            move |_, _, _| {
+                window.close();
+            }
+        ))
+        .build();
+    let actions = SimpleActionGroup::new();
+    actions.add_action_entries([close_action]);
+    window.insert_action_group("wlshud", Some(&actions));
+
+    let entry = SearchEntry::builder()
+        .hexpand(true)
+        .valign(gtk4::Align::Start)
+        .build();
+    let outer_box = Box::builder()
+        .orientation(gtk4::Orientation::Vertical)
+        .margin_bottom(APP_MARGIN)
+        .margin_top(APP_MARGIN)
+        .margin_end(APP_MARGIN)
+        .margin_start(APP_MARGIN)
+        .build();
+    outer_box.append(&entry);
+    // 2 rows inside
+    let top_row = Box::builder()
+        .orientation(gtk4::Orientation::Horizontal)
+        .vexpand(true)
+        .hexpand(true)
+        .build();
+    let bottom_row = Box::builder()
+        .orientation(gtk4::Orientation::Horizontal)
+        .hexpand(true)
+        .build();
+    outer_box.append(&top_row);
+    outer_box.append(&bottom_row);
+
+    let left_bar = Frame::builder().width_request(64).build();
+    let bottom_bar = Frame::builder()
+        // .orientation(gtk4::Orientation::Horizontal)
+        .height_request(64)
+        .hexpand(true)
+        .build();
+    let shortcuts_area = Frame::builder().hexpand(true).build();
+
+    let media_box = Frame::builder()
+        .width_request(300)
+        .height_request(200)
+        .build();
+    let notes_box = Frame::builder().width_request(400).build();
+
+    // populate two inner rows
+    top_row.append(&left_bar);
+    top_row.append(&shortcuts_area);
+    top_row.append(&notes_box);
+
+    bottom_row.append(&media_box);
+    bottom_row.append(&bottom_bar);
+
+    // close when unfocused
+    window.connect_is_active_notify(|window| {
+        if !window.is_active() {
+            let _ = <ApplicationWindow as WidgetExt>::activate_action(window, "wlshud.close", None);
         }
-    }
+    });
+    window.set_child(Some(&outer_box));
+    window.show();
 }
